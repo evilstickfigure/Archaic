c2475e449203fea37d8bfe8edcc58b24d2e1fbf7
thenoseofsauron <31170809+TheNoseOfSauron@users.noreply.github.com>
2020-05-06 10:52:51 +1000

    Some Stuff

--- src/main/java/com/evilstickfigure/archaic/item/weathered_battleaxe.java
+++ src/main/java/com/evilstickfigure/archaic/item/weathered_battleaxe.java
@@ -5,10 +5,11 @@ import com.evilstickfigure.archaic.RegistryHandler;
 import net.minecraft.item.*;
 import net.minecraftforge.common.ToolType;
 
-public class weathered_battleaxe extends Item {
+public class weathered_battleaxe extends AxeItem {
 
     public weathered_battleaxe() {
-        super(new Item.Properties().group(RegistryHandler.ARCHAIC_COMBAT_TAB));
+        // Same as Diamond, select AxeItem then find usages to see vanilla values
+        super(ItemTier.DIAMOND, 5.0F, -3.0F, new Item.Properties().group(RegistryHandler.ARCHAIC_COMBAT_TAB));
     }
 
 }
