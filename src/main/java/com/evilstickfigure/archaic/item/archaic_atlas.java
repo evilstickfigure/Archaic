3e68324dceea9b2c84b924d570f3a4332967a483
evilstickfigure <nozhaddon@gmail.com>
2020-05-06 15:46:30 +1000

    books got slightly bookier, but not bookish enough

--- src/main/java/com/evilstickfigure/archaic/item/archaic_atlas.java
+++ src/main/java/com/evilstickfigure/archaic/item/archaic_atlas.java
@@ -1,11 +1,22 @@
 package com.evilstickfigure.archaic.item;
 
 import com.evilstickfigure.archaic.RegistryHandler;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class archaic_atlas extends Item {
     public archaic_atlas() {
         super(new Item.Properties().group(RegistryHandler.ARCHAIC_GENERAL_TAB));
     }
-
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        ItemStack itemstack = playerIn.getHeldItem(handIn);
+        playerIn.openBook(itemstack, handIn);
+        playerIn.addStat(Stats.ITEM_USED.get(this));
+        return ActionResult.resultSuccess(itemstack);
+    }
 }
